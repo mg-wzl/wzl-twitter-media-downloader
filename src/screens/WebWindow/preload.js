629dcf3aae53f293a3d2cb9699295166ff6d4b05
MG Wzl <2665926+mg-wzl@users.noreply.github.com>
2022-12-10 16:13:13 +0400

    Get last tweet date on the page when the bottom of the feed is reached

--- src/screens/WebWindow/preload.js
+++ src/screens/WebWindow/preload.js
@@ -2,12 +2,16 @@ const { ipcRenderer } = require('electron');
 const events = require('../../events');
 const { writeJsonFile } = require('../../utils/fileUtils');
 const parser = require('../../parsers/tweetPageParser');
-const { parseComplexTweet } = require('../../parsers/inFeedTweetParser');
+const {
+  parseComplexTweet,
+  getDateOfTheLastTweetOnPage,
+} = require('../../parsers/inFeedTweetParser');
 const { scrapedFileNameFromUrl, parseTweetUrl } = require('../../utils/stringUtils');
 const uiLogger = require('../../utils/uiLogger');
 const { PageLoadingWorker } = require('../../PageLoadingWorker');
 
 let pageLoadingWorker;
+let targetTweetsFileName = '';
 
 ipcRenderer.on(events.CONTEXT_MENU_STOP_SCRAPING, (event, args) => {
   if (pageLoadingWorker) {
@@ -66,6 +70,24 @@ ipcRenderer.on(events.CONTEXT_MENU_SCROLL_AND_SCRAPE_CLICKED, (event, args) => {
   };
 
   const onFinished = () => {
+    const currentUrlStr = window?.location?.href;
+    let userHandle;
+    let lastTweetDate;
+    if (currentUrlStr) {
+      const currentUrl = new URL(currentUrlStr);
+      const pathParts = currentUrl.pathname.split('/').filter(Boolean);
+      if (
+        (pathParts.length === 1 && pathParts[0] !== 'search') ||
+        (pathParts.length === 2 && pathParts[1] === 'media')
+      ) {
+        // if there's only one path part - assuming it's the username
+        // if 2 and the last one is "media" - we're on media page
+        const userHandle = pathParts[0];
+        const lastTweetDate = getDateOfTheLastTweetOnPage(document);
+      }
+    }
+
+    console.log(`Reached the end of the page: ${currentUrlStr} ${userHandle}  ${lastTweetDate}`);
     console.log(
       `Colected ${linksArray.lengthFinished} links in ${
         (new Date().getTime() - startTime) / 1000
@@ -74,6 +96,7 @@ ipcRenderer.on(events.CONTEXT_MENU_SCROLL_AND_SCRAPE_CLICKED, (event, args) => {
     console.log('Total links scraped:', linksArray.length);
     writeJsonFile(linksArray, targetFolder, scrapedFileNameFromUrl(url));
     pageLoadingWorker = null;
+    ipcRenderer.send(events.FEED_PAGE_END_REACHED, {});
   };
 
   pageLoadingWorker = new PageLoadingWorker(
